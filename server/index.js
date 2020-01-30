373f521457443c7005990fefb863b1098d84d2b4
hay-0le <haley.olsen1@gmail.com>
2020-01-29 22:32:23 -0800

    Removed API logic into separate files

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
-require('newrelic');
 const express = require('express');
 const models = require('./models/models.js');
 const ItemDetails = require('./database/index.js');
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
-const port = 5555;
+const port = 4444;
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -17,46 +17,21 @@ app.use((req, res, next) => {
 });
 
 
+//GET one item
 app.get('/api/description/:productId', models.getOneItem);
 
-
-//GET all
+//GET all items
 app.get('/api/description', models.getAllItems);
 
+//POST create new item
+app.post('/api/description', models.addItem);
 
+//PUT update item information
+app.put('/api/description/:productId', models.updateItem);
 
-//POST
-app.post('/api/description', models.addItem)
-//PUT
-app.put('/api/description/:productId', models.updateItem)
-// (req, res) => {
-//   const id = req.params.productId;
+//DELETE one item
+app.delete('/api/description/:productId', models.deleteItem);
 
-//   models.updateItem(id, req.body)
-//     .then(results => {
-//       res.send(results);
-//       res.status(200)
-
-//     }).catch(err => {
-//       console.log(err)
-//       res.status(500);
-//       res.send('Item not updated');
-//     })
-
-// })
-
-//DELETE
-app.delete('/api/description/:productId', (req, res) => {
-  models.deleteItem(req.params.productId)
-    .then(results => {
-      res.send(results);
-
-    }).catch(err => {
-      console.log(err)
-      res.status(500);
-      res.send('Item not deleted')
-    })
-})
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
