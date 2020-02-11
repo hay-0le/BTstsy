05210ad11925146355f5642c237ce89e734b6bfa
hay-0le <haley.olsen1@gmail.com>
2020-02-10 18:11:37 -0800

    Removed native cache logic from client

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,17 +5,13 @@ import ShopPolicies from './ShopPolicies.jsx';
 import FaqList from './FaqList.jsx';
 import Messages from './Messages.jsx';
 import sampleData from '../sampleData.js';
-import { LRUCache } from '../../../server/database/LRUcache.js';
-
-
 
 function App() {
   const sample = sampleData.sampleData[1];
   const [description, getDescription] = useState(sample.productid, sample.productdescription);
   const [data, setData] = useState(sample);
 
-  //cache to hold top 25 most recently searched queries
-  const [cache, setCache] = useState(new LRUCache(25));
+
 
   console.log("First time", cache)
 
@@ -28,24 +24,10 @@ function App() {
 
   async function fetchData(productId) {
 
-    let cachedItem = cache.get(productId);
-    console.log("cached item", cachedItem);
-
-    if (cachedItem) {
-      setData(cachedItem);
-
-    } else {
 
       const res = await axios.get(`/api/description/${productId}`);
       const tempData = res.data[0];
-
-      console.log("hellooo", cache);
-      let updatedCache = cache.set(productId, tempData);
-      console.log(updatedCache);
-      setCache(updatedCache);
-      console.log("after cached", cache)
-      setData(tempData);
-    }
+      return tempData;
 
 
   }
